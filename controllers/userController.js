151f89e4fc87cdb8e2f73e6f491cb734fe5b0f07
lakb248 <lakb248@163.com>
2014-08-10 20:32:03 +0800

    login complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,19 @@
 var User = require('../models/user') ;
 //add user
 exports.postUser = function(req,res){
-	console.log({
-		username: req.body.username ,
-		password: req.body.password ,
-		token : req.body.username + ':' + req.body.password
-	}) ;
+
 	//instance a user
 	var user = new User({
 		username: req.body.username ,
 		password: req.body.password ,
-		token : req.body.username + ':' + req.body.password
+		token : new Date().getTime()
 	}) ;
 	//save user
 	user.save(function(err){
 		if(err){
-			res.send(err) ;
+			res.json({'result':{'status':'-1'}}) ;
 		}else{
-			res.json({'message':'success'}) ;
+			res.json({'result':{'status':'1','userId':user._id,'token':user.token}}) ;
 		}
 	}) ;
 } ;
@@ -28,10 +24,10 @@ exports.updateUser = function(req,res){
 		{username:req.body.username , password:req.body.password},
 		function(err,num,raw){
 			if(err){
-				res.send(err) ;
+				res.json({'result':{'status':'-1'}}) ;
+			}else{
+				res.json({'result':{'status':'1'}}) ;
 			}
-
-			res.json({'message':'success'}) ;
 		}
 	);
 } ;
@@ -39,18 +35,64 @@ exports.updateUser = function(req,res){
 exports.getUser = function(req,res){
 	User.find({_id:req.param.userId},function(err,user){
 		if(err){
-			res.send(err) ;
+			res.json({'result':{'status':'-1'}}) ;
+		}else{
+			res.json({'result':{'status':'1','user':user}}) ;
 		}
 
-		res.json(user) ;
 	}) ;
 } ;
 //user login
 exports.login = function(req,res){
-	User.find({username:req.body.username,password:req.body.username},function(err,user){
-		if(err){
-			res.json({'message':'-1'}) ;
-		}
-		res.json({'message':user._id}) ;
-	}) ;
+	if(req.body.token){
+		//find user by token
+		User.find({token:req.body.token},function(err,users){
+			if(err){
+				res.json({'result':{'status':'-1'}}) ;
+			}else if(users.length <= 0){
+				//there is no user whose token is equal to token in request,login failed
+				res.json({'result':{'status':'0','message':'error'}}) ;
+			}else{
+				//login success , update token
+				var newToken = new Date().getTime() ;
+				User.update(
+					{_id:users[0]._id} ,
+					{token:newToken} ,
+					function(err,num,raw){
+
+						if(err){
+							res.json({'result':{'status':'-1'}}) ;
+						}else{
+							res.json({'result':{'status':'1','userId':users[0]._id,'token':newToken}}) ;
+						}
+					}
+				) ;
+			}
+		}) ;
+	}else{
+		User.find({username:req.body.username,password:req.body.password},function(err,users){
+			if(err){
+				res.json({'result':{'status':'-1'}}) ;
+			}else if(users.length <= 0){
+				//there is no user whose token is equal to token in request,login failed
+				res.json({'result':{'status':'0','message':'error'}}) ;
+			}else{
+				var newToken = new Date().getTime() ;
+				User.update(
+					{_id:users[0]._id} ,
+					{token:newToken} ,
+					function(err,num,raw){
+
+						if(err){
+							res.json({'result':{'status':'-1'}}) ;
+						}else{
+							res.json({'result':{'status':'1','userId':users[0]._id,'token':newToken}}) ;
+						}
+					}
+				) ;
+			}
+			
+		}) ;
+	}
+	
 } ;
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 var couples = angular.module('couples', ['ionic','ui.router']) ;
 
-
+//config router
 couples.config(function($stateProvider,$urlRouterProvider){
 	$urlRouterProvider.otherwise('/index') ;
 	$stateProvider
@@ -32,89 +28,174 @@ couples.config(function($stateProvider,$urlRouterProvider){
 		});
 }) ;
 
-couples.run(function($rootScope){
-	$rootScope.isLoaded = true ;
-}) ;
+//config services
 
-//Controller
-
-var couplesController = couples.controller('couplesController',function($scope,$location,$state){
-	//$scope.isLoaded = false ;
-	$scope.register = function(){
-		// $location.path('/register') ;
-		$state.go('register') ;
-	} ;
-}) ;
-
-var loginController = couples.controller('loginController', ['$scope','UserService',function($scope,userService){
-  	$scope.username = '' ;
-  	$scope.password = '' ;
-  	$scope.login = function(){
-  		if($scope.username.length === 0){
-  			alert('Please Enter Your Username') ;
-  		}else if($scope.password.length === 0){
-  			alert('Please Enter Your Password') ;
-  		}else{
-  			var token = userService.login($scope.username,$scope.password) ;
-  		}
-
-  	} ;
-  
-}]) ;
-
-var registerController = couples.controller('registerController',['$scope','UserService',function($scope,userService){
-	$scope.register = function(){
-		//alert('username:' + $scope.username + ';password:' + $scope.password) ;
-		if($scope.password !== $scope.confirmPass){
-			alert('different password') ;
-		}else{
-			userService.register($scope.username,$scope.password) ;
-		}
-
-	} ;
-}]) ;
-/************************Service*************************/
 /******************UserService***********************/
-couples.factory('UserService', function($http){
+couples.factory('UserService', ['$http',function($http){
 	return {
-		register:function(username,password){
+		register:function(username,password,success,fail){
 
 			$http.post(
 				'/api/register',
 				{username:username,password:password}
-			).success(function(data){
-				console.log(data.message) ;
-				if(data.message === 'success'){
-					alert('success') ;
-				}else{
-					alert('fail') ;
-				}
-			}).error(function(status,data){
-				alert(data) ;
-			}) ;
+			).success(success).error(fail) ;
 		} ,
-		updateUser:function(username,password,userId){
+		loginByUsername:function(username,password,success,fail){
+			$http.post(
+				'/api/login' ,
+				{username:username,password:password} 
+			).success(success).error(fail) ;
+		} ,
+		loginByToken:function(token,success,fail){
+			$http.post(
+				'/api/login' ,
+				{token:token} 
+			).success(success).error(fail) ;
+		} ,
+		updateUser:function(user,success,fail){
 			$http.post(
 				'/api/updateUser',
-				{username:username,password:password,userId:id}
-			).success(function(data){
-				console.log(data) ;
-			}).error(function(status,data){
-				console.log(data) ;
-			}) ;
+				{username:user.username,password:user.password,userId:user.id}
+			).success(success).error(fail) ;
 
 		} ,
-		getUser:function(id){
+		getUser:function(id,success,fail){
 			$http.get(
 				'/api/getUser',
 				{userId:id}
-			).success(function(data){
-				console.log(data) ;
-			}).error(function(status,data){
-				console.log(data) ;
-			}) ;
+			).success(success).error(fail) ;
 		}
 	}
-}) ;
+}]) ;
+// module run block
+couples.run(['$rootScope','$state','$location','$timeout','UserService',
+	function($rootScope,$state,$location,$timeout,UserService){
+	//login by token
+	var token = localStorage.getItem('token') ;
+	$rootScope.isLoaded = false ;
+	if(token === null || token === undefined){
+		//no token
+		$location.path('/login') ;
+	}else{
+		//check token
+		UserService.loginByToken(
+			token ,
+			function(data){
+				var result = data.result ;
+				if(result.status === '-1'){
+					//out of data , clear the token
+					localStorage.removeItem('token') ;
+					$location.path('/login') ;
+				}else if(result.status === '0'){
+					alert('user does not exist') ;
+					$location.path('/') ;
+				}else{
+					//login success,update token
+					$rootScope.userId = result.userId ;
+					localStorage.setItem('token',result.token) ;
+					$location.path('/') ;
+				}
+			} ,
+			function(status,data){
+				alert(data) ;
+			}
+		) ;
+	} ;
+	$timeout(function(){$rootScope.isLoaded = true ;},1000) ;
+}]) ;
 
+//Controller
 
+var couplesController = couples.controller(
+	'couplesController',
+	['$scope','$location','$state','UserService',
+	function($scope,$location,$state,UserService){
+		//$scope.isLoaded = false ;
+		$scope.register = function(){
+			// $location.path('/register') ;
+			$state.go('register') ;
+		} ;
+}]) ;
+
+var loginController = couples.controller(
+	'loginController', 
+	['$rootScope','$scope','UserService','$location',
+	function($rootScope,$scope,userService,$location){
+	  	$scope.username = '' ;
+	  	$scope.password = '' ;
+	  	$scope.login = function(){
+	  		if($scope.username.length === 0){
+	  			alert('Please Enter Your Username') ;
+	  		}else if(!/^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test($scope.username)){
+	  			alert('Please Enter The Correct Email') ;
+	  		}else if($scope.password.length === 0){
+	  			alert('Please Enter Your Password') ;
+	  		}else if($scope.password.length < 8){
+	  			alert('Your Password Must Over 8') ;
+	  		}else{
+	  			//login by username
+	  			userService.loginByUsername(
+	  				$scope.username , 
+	  				$scope.password ,
+	  				function(data){
+	  					var result = data.result ;
+	  					if(result.status === '-1'){
+							alert('fail') ;
+						}else if(result.status === '0'){
+							//username or password is incorrect
+							alert('user does not exist') ;
+						}else{
+							//login success,update token
+							$rootScope.userId = result.userId ;
+							localStorage.setItem('token',result.token) ;
+							$location.path('/') ;
+						}
+	  				} ,
+	  				function(status,data){
+	  					alert(data) ;
+	  				}
+	  			) ;
+	  		}
+
+	  	} ;
+  
+}]) ;
+
+var registerController = couples.controller(
+	'registerController',
+	['$rootScope','$scope','UserService','$location',
+	function($rootScope,$scope,userService,$location){
+		$scope.register = function(){
+			//alert('username:' + $scope.username + ';password:' + $scope.password) ;
+			if($scope.username.length === 0){
+	  			alert('Please Enter Your Username') ;
+	  		}else if(!/^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test($scope.username)){
+	  			alert('Please Enter The Correct Email') ;
+	  		}else if($scope.password.length === 0){
+	  			alert('Please Enter Your Password') ;
+	  		}else if($scope.password.length < 8){
+	  			alert('Your Password Must Over 8') ;
+	  		}else if($scope.password !== $scope.confirmPass){
+				alert('different password') ;
+			}else{
+				userService.register(
+					$scope.username ,
+					$scope.password ,
+					function(data){
+						var result = data.result ;
+						if(result.status === '-1'){
+							alert('fail') ;
+						}else{
+							$rootScope.userId = result.userId ;
+							localStorage.setItem('token',result.token) ;
+							$location.path('/') ;
+						}
+					} ,
+					function(status,data){
+						alert(data) ;
+					}
+				) ;
+			}
+
+		} ;
+}]) ;
